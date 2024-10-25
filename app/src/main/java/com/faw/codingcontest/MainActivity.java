f54c95521140b127e963c4f16257e8c91011c374
caichengxuan <caichengxuan@faw.com.cn>
2024-10-25 19:54:24 +0800

    fix：建立四个不同的activity

--- app/src/main/java/com/faw/codingcontest/MainActivity.java
+++ app/src/main/java/com/faw/codingcontest/MainActivity.java
@@ -2,13 +2,33 @@ package com.faw.codingcontest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button mBtnCCX, mBtnSJS, mBtnJLJ, mBtnMCJ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnCCX = findViewById(R.id.btn_ccx);
+        mBtnSJS = findViewById(R.id.btn_sjs);
+        mBtnJLJ = findViewById(R.id.btn_jlj);
+        mBtnMCJ = findViewById(R.id.btn_mcj);
+        mBtnCCX.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, OneActivity.class));
+        });
+        mBtnSJS.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, TwoActivity.class));
+        });
+        mBtnJLJ.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, ThreeActivity.class));
+        });
+        mBtnMCJ.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, FourActivity.class));
+        });
     }
 }
